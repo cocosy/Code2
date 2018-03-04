fc679097722bec6b4fda4587f6d1654c30ccd441
Yang Shen <cocosuiban@gmail.com>
2018-03-04 12:37:22 -0500

    final

--- week05_paint/w5_02_choose_adventure_data/sketch.js
+++ week05_paint/w5_02_choose_adventure_data/sketch.js
@@ -65,20 +65,20 @@ function Scene(sceneText, options, nextScenes,x,y,r,hair) {
     
     
     // arc(this.x,this.y+110, 210, 180,-PI,PI*2,HALF_PI);
-    for (var i = 0; i < this.hair; i++) {
+    for (var i = 0; i < this.hair+1; i++) {
     for (var j = 0; j < this.hair; j++) {
    strokeWeight(5);
-    line(this.x-50+i*20+j*155,this.y-60,this.x-70+i*30+j*157,this.y-120);
+    line(this.x-50+j*28+i*155,this.y-60,this.x-70+j*40+i*157,this.y-100);
   }
    strokeWeight(2);
-    fill(this.hair*25,this.x/2,this.y/2);
+    fill(this.r+100,this.x/2,this.y/2);
     ellipse(this.x+i*152,this.y,150,150);
     fill(255);
     ellipse(this.x+25+i*152,this.y-20,20,20);
     ellipse(this.x-25+i*152,this.y-20,20,20);
    strokeWeight(2);
   
-    arc(this.x+i*152,this.y+20,90,60,radians(180)-radians(this.r),radians(this.r),CHORD);
+    arc(this.x+i*152,this.y+30,100,60,radians(180)-radians(this.r),radians(this.r),CHORD);
   }
     // arc(this.x,this.y+110, 210, 180,-PI,TWO_PI,HALF_PI);
     //0,PI smile; PI - 0
