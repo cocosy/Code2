d38dc130bd3a4bebccb4f24f061391c49a1bd0b3
Yang Shen <cocosuiban@gmail.com>
2018-03-03 21:29:18 -0500

    update

--- week05_paint/w5_02_choose_adventure_data/sketch.js
+++ week05_paint/w5_02_choose_adventure_data/sketch.js
@@ -40,14 +40,16 @@ function draw() {
 
 function CreateScenesFromData(data) {
   for (var i = 0; i < data.length; i++) {
-    scenes.push(new Scene(data[i].sceneText, data[i].options, data[i].nextScenes))
+    scenes.push(new Scene(data[i].sceneText, data[i].options, data[i].nextScenes, data[i].position.x,data[i].position.y))
   }
 }
 
-function Scene(sceneText, options, nextScenes) {
+function Scene(sceneText, options, nextScenes, position.x,posiiton.y) {
   this.sceneText = sceneText;
   this.options = options;
   this.nextScenes = nextScenes;
+  this.position.x= position.x;
+  this.position.y = position.y;
 
   this.display = function() {
     fill(0);
@@ -57,6 +59,7 @@ function Scene(sceneText, options, nextScenes) {
     for (var i = 0; i < options.length; i++) {
       text('OPTION ' + (i + 1) + ': ' + this.options[i], 150, 200 + i * 50);
     }
+    ellipse(50,50,this.position.x,this.position.y);
   }
 }
 
