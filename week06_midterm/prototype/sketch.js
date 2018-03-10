7095b41a708ff24465bbc4e03ab5a7b08ffbdee2
Yang Shen <cocosuiban@gmail.com>
2018-03-10 15:51:23 -0500

    prototype

---
+++ week06_midterm/prototype/sketch.js
@@ -0,0 +1,71 @@
+// code 2
+// section a
+// bfa dt
+// spring 2018
+// bryan ma
+
+// week 5
+// choose your own adventure data
+
+// scene data model: 
+
+// {
+//   sceneText: '', //the scene text
+//   options: [], // the text options to choose
+//   nextScenes: []  // the target scene based on the previous options
+// }
+
+var sceneData;
+
+var currentScene = 0;
+var scenes = [];
+
+//
+function preload() {
+  sceneData = loadJSON('scenes.json');
+}
+
+function setup() {
+  createCanvas(800, 800);
+  CreateScenesFromData(sceneData.scenes);
+}
+
+function draw() {
+  background(231, 190, 150);
+  scenes[currentScene].display();
+
+  fill(0);
+  textSize(24);
+  text("press the option number to advance to the indicated scene", 50, 700);
+}
+
+function CreateScenesFromData(data) {
+  for (var i = 0; i < data.length; i++) {
+    scenes.push(new Scene(data[i].sceneText, data[i].options, data[i].nextScenes))
+  }
+}
+
+function Scene(sceneText, options, nextScenes) {
+  this.sceneText = sceneText;
+  this.options = options;
+  this.nextScenes = nextScenes;
+
+  this.display = function() {
+    fill(0);
+    textSize(32);
+    text(this.sceneText, 100, 100);
+
+    for (var i = 0; i < options.length/5; i++) {
+      for(var j =0; j<options.length/3; j++){
+      rect(50+i*50,50+i*50,20,50);}
+    }
+  }
+}
+
+function keyPressed() {
+  var numberPressed = parseInt(key);
+  var newScene = scenes[currentScene].nextScenes[numberPressed - 1];
+  if (newScene !== undefined) {
+    currentScene = newScene;
+  }
+}
