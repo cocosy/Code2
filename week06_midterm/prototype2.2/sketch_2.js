d49208c96d57205d0e313d15e0efe7b92517ffcb
Yang Shen <cocosuiban@gmail.com>
2018-03-13 12:29:54 -0400

    2.2

---
+++ week06_midterm/prototype2.2/sketch_2.js
@@ -0,0 +1,102 @@
+// code 2
+// bfa dt
+// spring 2018
+
+// week 6 mid-term
+
+
+var textData;
+// var card;
+// var cards;
+
+
+function preload() {
+  textData = loadJSON("text.json");
+}
+
+
+function setup() {
+	createCanvas(800, 700);
+	  fill(0);
+	  	textSize(20);
+	// CreateTextFromData(textData.card);
+
+}
+
+ function draw() {
+
+	    fill(255);
+	
+ 		textAlign(CENTER, CENTER);
+	 for (var i = 0; i < textData.card.length; i++) {
+	 text(textData.card[i],400,100+20*i);
+	 }
+// 	Card.display();
+
+// }
+
+// function CreateTextFromData(data) {
+//   for (var i = 0; i < data.length; i++) {
+//     cards.push(new Card(data[i].card)）;
+//   }
+ }
+
+
+// function Card(cards){
+// 	this.width = width - 100;
+// 	this.height = height - 50;
+// 	this.text = cards;
+
+	
+
+// 	this.display = function () {
+
+// 		rect(100,50,this.width, this.height);
+// 		fill(255);
+// 		textSize(28);
+// 		textAlign(CENTER, CENTER);
+// 		for (var j = 0; j < text.length; j++) {
+// 		text(this.text[j],width/2,height/2+15*j);
+
+// 	}}
+// }
+
+
+
+// function drawScene(whichScene) {
+// 	switch (currentState) {
+// 		case sceneState.TUTORIAL:
+
+// 			background(150, 200, 200);
+// 			fill(0);
+// 			textSize(28);
+// 			textAlign(CENTER, CENTER);
+// 			text("Wecome to play this little drinking game",width/2,height/2-100);
+// 			text("Hope your like it",width/2,height/2);
+
+// 				break;
+// 				default:
+// 					break;
+// 			}
+// 	}
+
+// 	function checkTransition(whichScene) {
+// 		switch (whichScene) {
+// 			case sceneState.TUTORIAL:
+// 				if (keyOn) {
+// 					currentState++;
+// 					setUpScene(currentState);
+// 				}
+				
+// 				break;
+// 			default:
+// 				break;
+// 		}
+// 	}
+
+
+
+
+
+
+
