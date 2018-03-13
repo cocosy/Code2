604761916ba574c7243f0829fa64b1324add6cf7
Yang Shen <cocosuiban@gmail.com>
2018-03-13 15:49:34 -0400

    push

--- week06_midterm/prototype2.2/sketch_2.js
+++ week06_midterm/prototype2.2/sketch_2.js
@@ -6,7 +6,7 @@
 
 
 var textData;
-// var card;
+ // var card;
  var cardArray = [];
 
 
@@ -17,25 +17,29 @@ function preload() {
 
 function setup() {
 	createCanvas(800, 700);
-	  fill(0);
-	  	textSize(20);
+	fill(0);
+	textSize(20);
 	 for (var i = 0; i < textData.card.length; i++) {
-	 var newCard = new Card (textData.card[i]);
-	 cardArray.push(newCard);
-	 }
+   cardArray.push = new Card(textData.card[i]);
+}
+
+	
 	// CreateTextFromData(textData.card);
 
 }
+
+
 
  function draw() {
 
 	    fill(255);
 	
- 		textAlign(CENTER, CENTER);
-
- 		for (var i = 0; i < textData.card.length; i++) {
-        cardArray[i].display();
-    }
+ 	textAlign(CENTER, CENTER);
+ 	 for (var i = 0; i < textData.card.length; i++) {
+ 	// r = int(random(0,cardArray.length));
+    cardArray[i].display();
+}
+}
 // 	Card.display();
 
 // }
@@ -44,7 +48,6 @@ function setup() {
 //   for (var i = 0; i < data.length; i++) {
 //     cards.push(new Card(data[i].card)）;
 //   }
- }
 
 
 function Card(cardArray){
@@ -57,14 +60,15 @@ this.text = cardArray;
  	this.display = function () {
 
 // 		rect(100,50,this.width, this.height);
-		fill(255);
-		textSize(15);
-		textAlign(CENTER, CENTER);
-for (var j = 0; j < text.length; j++) {
-text(this.text[j],100,100+30*j);
+    r = int(random(0,cardArray.length));
+	fill(255);
+	textSize(15);
+	textAlign(CENTER, CENTER);
+	text(this.text[r],100,100+30*i);
 
-	}}
+	}
 }
+
 
 
 
