0f673748a30a9ae232ed9593f25c45e27de543aa
Yang Shen <cocosuiban@gmail.com>
2018-04-08 12:16:47 -0400

    update

--- week08_api/sketch.js
+++ week08_api/sketch.js
@@ -3,7 +3,7 @@
 var api = "http://api.giphy.com/v1/gifs/search?";
 var apiKey ="&api_key=5n6MRuAtrByEc8M5piZdv66Y1Jvc4Igr";
 var query = "&q=rick+morty";
-
+var img;
 
 
 /*
@@ -26,8 +26,12 @@ function setup(){
 
 
 
-function gotData(data){
- print(data.data[0].images.original.url);
+function gotData(giphy){
+	for(var i = 0;i<giphy.data.length;i++){
+ createImg(giphy.data[i].images.original.url);
+
+
+}
 }
 
 
